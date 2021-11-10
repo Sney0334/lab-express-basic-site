922d97ed2736953343c869f7288eab297340b778
aniljogireddy <anil.jogireddy@gmail.com>
2021-11-10 14:37:39 +0100

    done

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 
 const app = express();
+//view engine setup
+app.set("view engine", "hbs")
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.all("/home", (request, response) => {
-    response.sendFile(path.join(__dirname, "views", "home.html"))
+    response.render("home")
 })
 
 app.all("/about", (request, response) => {
-    response.sendFile(path.join(__dirname, "views","about.html"))
+    response.render("about")
 })
 app.all("/cast", (request, response) => {
-    response.sendFile(path.join(__dirname, "views","cast.html"))
+    response.render("cast")
 })
 
 app.all("/gallery", (request, response) => {
-    response.sendFile(path.join(__dirname, "views","gallery.html"))
+    response.render("gallery")
 })
 
 app.listen(3000, ()=>{
